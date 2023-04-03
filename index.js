1ec99947f2484d99be0a7b8f6714dfdc94833be7
luisparedes29 <luisparedescrv@gmail.com>
2023-04-03 12:01:59 -0400

    Funcionalidad casi completa, faltan estilos, debounce, responsive y claro/oscuro

---
+++ index.js
@@ -0,0 +1,75 @@
+const input = document.querySelector('#search');
+const userList = document.querySelector('.user-list');
+const form = document.querySelector('#form');
+const URL = 'https://api.github.com/search/users?q=';
+const URL_USUARIO = 'https://api.github.com/users/';
+const URL_REPOS = 'https://api.github.com/users/';
+
+form.addEventListener('submit', (e) => {
+    userList.innerHTML = ``;
+    e.preventDefault();
+    search = input;
+    console.log(search.value);
+    buscar(search);
+});
+
+function buscar(search) {
+    fetch(`${URL}${search.value}`)
+        .then((data) => data.json())
+        .then((users) => {
+            if (users.total_count === 0) {
+                alert('Usuario no encontrado');
+            }
+            console.log(users);
+            const user = users.items.slice(0, 3);
+            console.log(user);
+            user.forEach((u) => {
+                let element = document.createElement('article');
+                element.classList.add('contenedorTarjeta');
+                let fig = document.createElement('figure');
+                fig.classList.add('contenedorImg');
+                let imgUsuario = document.createElement('img');
+                imgUsuario.classList.add('imgUsuario');
+                console.log(u.login);
+                let contSecundario = document.createElement('div');
+                contSecundario.classList.add('contSecundario');
+                let nombreUsuario = document.createElement('p');
+                nombreUsuario.classList.add('nombre');
+                let username = document.createElement('p');
+                username.classList.add('username');
+                let repositorios = document.createElement('p');
+                let company = document.createElement('p');
+                company.classList.add('company');
+
+                fetch(`${URL_USUARIO}${u.login}`)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        console.log(data);
+
+                        imgUsuario.src = `${data.avatar_url}`;
+                        username.innerHTML = `<span class=username>${u.login}</span> `;
+                        repositorios.innerHTML = `Repositorios: <span class="numRepo">${data.public_repos}</span>`;
+                        userList.appendChild(element);
+                        element.appendChild(fig);
+                        fig.appendChild(imgUsuario);
+                        element.appendChild(contSecundario);
+                        contSecundario.appendChild(username);
+                        if (data.name) {
+                            nombreUsuario.innerHTML = `<span class="nombre">${data.name}</span> `;
+                            contSecundario.appendChild(nombreUsuario);
+                        } else {
+                            nombreUsuario.innerHTML = `name: <span class="name">N/A</span>`;
+                            contSecundario.appendChild(nombreUsuario);
+                        }
+                        if (data.company) {
+                            company.innerHTML = `Company: <span class="company">${data.company}</span>`;
+                            contSecundario.appendChild(company);
+                        } else {
+                            company.innerHTML = `Company: <span class="company">N/A</span>`;
+                            contSecundario.appendChild(company);
+                        }
+                        contSecundario.appendChild(repositorios);
+                    });
+            });
+        });
+}
